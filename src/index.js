8a1d619e6ef5089887a7107dd96705d5c9a59db1
ssharkunova <sharkunova@yandex.ru>
2021-03-08 11:00:45 +0300

    implement function Reverse-Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+   if (n < 0) {
+      n = Math.abs(n);
+    }
+    if (n%10 === 0) {
+      n = n/10;
+    }
+    let newNumber = Number(String(n).split('').reverse().join(''));
+    return newNumber;  
 }
